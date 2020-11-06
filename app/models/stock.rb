771bc06a30ad7d2a7df90b84fece9b56b1d13cc0
JeffreyGallegosCoding <jeffreygallegos01@gmail.com>
2020-11-06 11:32:29 -0700

    Added many to many association for users and stocks and stock display table on portfolio page

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,9 @@
 class Stock < ApplicationRecord
+  has_many :user_stocks
+  has_many :users, through: :user_stocks
+
+  # makes sure to have a valid symbol when saving to database
+  validates :name, :ticker, presence: true
 
   def self.new_lookup(ticker_symbol)
     client = IEX::Api::Client.new(
