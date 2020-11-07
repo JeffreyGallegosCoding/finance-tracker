a4ff46354eb6df80cbf1ca4d77fe0d8470de7b23
JeffreyGallegosCoding <jeffreygallegos01@gmail.com>
2020-11-07 16:05:20 -0700

    Added base friend search set up and templates

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,31 @@
 class UsersController < ApplicationController
   def my_portfolio
-    @tracked_stocks = current_user.stocks
+    @tracked_friends = current_user.friends
   end
 
   def friends
     @friends = current_user.friends
+  end
+
+  def search
+    if params[:friend].present?
+      @friend = params[:friend]
+      if @friend
+        respond_to do |format|
+          format.js { render partial: 'users/friend_result' }
+        end
+      else
+        respond_to do |format|
+          flash.now[:alert] = "Could not find user"
+          format.js { render partial: 'users/friend_result' }
+        end
+      end
+    else
+      respond_to do |format|
+        flash.now[:alert] = "Please enter in a valid friend name or email to search"
+        format.js { render partial: 'users/friend_result' }
+      end
+    end
   end
 
 end
